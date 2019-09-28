5b42bf3b853e0089e387755020d12dcee18e216e
Heesoon Kim <heesoon0823@gmail.com>
2019-09-28 19:13:40 +0900

    orientation rivision - use settimeout

--- js/GLGraphics.js
+++ js/GLGraphics.js
@@ -172,7 +172,23 @@ var facialAnimation = function(){
 		window.addEventListener("orientationchange", function() {
     		/* 向き切り替え時の処理 */
 
+	    	setTimeout(function(){
+
+		    	if (window.innerHeight > window.innerWidth) {
+		        	/* 縦画面時の処理 */
+		        	mesh.position.set(0,0,0);
+		    		console.log("vertical");
+
+				} else {
+		    		/* 横画面時の処理 */
+		    		mesh.position.set(0,-200,0);
+		        	console.log("horizontal");
+				}
+
+			}, 100);
+
 		});
+
 	}
 
 	
@@ -203,17 +219,6 @@ var facialAnimation = function(){
 		renderer.setPixelRatio( window.devicePixelRatio );
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		composer.setSize( window.innerWidth, window.innerHeight );
-
-	    if (window.innerHeight > window.innerWidth) {
-        	/* 縦画面時の処理 */
-        	mesh.position.set(0,0,0);
-    		console.log("vertical");
-		} else {
-    		/* 横画面時の処理 */
-
-    		mesh.position.set(0,-200,0);
-        	console.log("horizontal");
-		}
 
 
 
